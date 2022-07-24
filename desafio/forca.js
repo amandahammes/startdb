bf43525ef4afc64496950f522897048560b9ff50
amandahammes <82393453+amandahammes@users.noreply.github.com>
2022-07-24 14:13:54 -0300

    jogo da forca

---
+++ desafio/forca.js
@@ -0,0 +1,39 @@
+class Forca {
+
+  vidas = 6;
+  letrasChutadas = [];
+
+  constructor(palavra) {
+    this.palavraCerta = palavra.split("");
+    this.palavra = palavra.split("").map(e => e = "_");
+  }
+
+  chutar(letra) {
+    if (! (letra.length === 1)) return;
+    if (! (this.letrasChutadas.indexOf(letra) < 0)) return;
+    this.letrasChutadas.push(letra);
+    if ((this.palavraCerta.indexOf(letra)< 0)) {
+      return this.vidas = this.vidas -1;
+    }
+    this.palavraCerta.forEach((e, i) => {
+      if (!(e === letra )) return;
+      this.palavra[i]=letra;
+    })
+  }
+
+  buscarEstado() { 
+    if(this.vidas === 0){return "perdeu"};
+    if(this.palavra.indexOf("_") < 0){return "ganhou"};
+    return "Aguardando chute";
+  }
+
+  buscarDadosDoJogo() {
+      return {
+          letrasChutadas: this.letrasChutadas, 
+          vidas: this.vidas, 
+          palavra: this.palavra, 
+      }
+  }
+}
+
+module.exports = Forca;
